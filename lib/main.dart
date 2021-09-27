aeb271b9b244766dd9a4abdc4953583ecb57a73b
Nikita Vyzhol <flamenv22@gmail.com>
2021-09-27 21:52:27 +0300

    Added files for my linkedin clone

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,302 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LinkedIn clone',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.grey,
+          scaffoldBackgroundColor: const Color(0xF3F2EFEF)),
+      home: Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(LinkedInIcon.icon)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.people)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.work)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.chat)),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.notifications)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz)),
+            ],
+          ),
+          body: const SingleChildScrollView(child: Page())),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Page extends StatelessWidget {
+  const Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return Column(
+      children: [
+        const Padding(padding: EdgeInsets.only(top: 10)),
+        const Profile(),
+        Directionality(
+            textDirection: TextDirection.rtl,
+            child: TextButton.icon(
+              onPressed: () {},
+              label: Text('Развернуть'),
+              icon: Icon(Icons.keyboard_arrow_down),
+              style: TextButton.styleFrom(primary: Colors.black),
+            )),
+        const PostNav(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const Text('Сортировать:'),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_drop_down),
+                  label: const Text('Популярные',
+                      style: TextStyle(color: Colors.black))),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        const AddPeople()
+      ],
     );
   }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFEFEFEFE),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 15)),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  CircleAvatar(
+                    radius: 37,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 35,
+                      backgroundImage: AssetImage('assets/avatar.jpg'),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: const [
+                  Padding(padding: EdgeInsets.only(top: 25)),
+                  Text(
+                    'Nikita Vyzhol',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text(
+                    'Student at Lviv Polytechnic National University (Internet of Things program)',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 15)),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PostNav extends StatelessWidget {
+  const PostNav({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFEFEFEFE),
+      child: Row(
+        children: [
+          const Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+          Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('assets/avatar.jpg'),
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 10)),
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        labelText: 'Новая публикация',
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              Row(
+                children: [
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.photo),
+                      label: Text('Фото')),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.play_circle_fill),
+                      label: Text('Видео')),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.calendar_today),
+                      label: Text('Мероприятие')),
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.article),
+                      label: Text('Написать статью')),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AddPeople extends StatelessWidget {
+  const AddPeople({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFEFEFEFE),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              const Text(
+                'Nikita, хотите получать еще больше \n обновлений?',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              const Text(
+                  'Добавьте людей, которых вы можете знать, в свою сеть \n контактов и просматривайте их обновления в ленте.'),
+              const Padding(padding: EdgeInsets.only(bottom: 30)),
+              Row(
+                children: [
+                  Card(
+                    child: Column(
+                      children: [
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/neo.jpg'),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 20)),
+                        const Text(
+                          'Thomas A. \n Anderson',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 10)),
+                        const Text('Chosen by Matrix'),
+                        const Padding(padding: EdgeInsets.only(top: 50)),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: const Text('Установить \n контакт'),
+                          style: OutlinedButton.styleFrom(
+                            primary: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/john.jpg'),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 20)),
+                        const Text(
+                          'John Wick',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 10)),
+                        const Text('Someone killed \n his dog'),
+                        const Padding(padding: EdgeInsets.only(top: 50)),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: const Text('Установить \n контакт'),
+                          style: OutlinedButton.styleFrom(
+                            primary: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/geralt.png'),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 20)),
+                        const Text(
+                          'Geralt of Rivia \n',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 10)),
+                        const Text('Struggles with C++'),
+                        const Padding(padding: EdgeInsets.only(top: 50)),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: const Text('Установить \n контакт'),
+                          style: OutlinedButton.styleFrom(
+                            primary: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LinkedInIcon {
+  LinkedInIcon._();
+
+  static const _kFontFam = 'LinkedInIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData icon =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
